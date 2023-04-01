b958264b60f9d84f05d434fdb31bece9cac726ff
volazara <mk.one.rh@gmail.com>
2023-04-02 01:45:05 +0200

    sequenchang

--- src/helper/helper.js
+++ src/helper/helper.js
@@ -50,7 +50,7 @@ exports.messageReC = (userUid) => {
     )
 }
 
-exports.findInbox = async (sender, recever) => {
+/*exports.findInbox = async (sender, recever) => {
     return await participe.findAll({
         attributes: ['inbox_id',
             [sequelize.literal('(SELECT COUNT(*) FROM participes AS p3 where p3.inbox_id = participe.inbox_id)'),
@@ -68,7 +68,27 @@ exports.findInbox = async (sender, recever) => {
         having: sequelize.literal('count = 2')
     })
 
-}
+}*/
+
+exports.findInbox = async (sender, receiver) => {
+    const result = await participe.findAll({
+        attributes: ['inbox_id', [sequelize.fn('COUNT', sequelize.col('p2.id')), 'count']],
+        where: {
+            user_uid: sender
+        },
+        include: [{
+            model: participe,
+            as: 'p2',
+            where: {
+                user_uid: receiver
+            }
+        }],
+        group: ['participe.inbox_id'],
+        having: sequelize.literal('count = 2')
+    });
+
+    return result;
+};
 
 
 exports.cache = new NodeCache({ stdTTL: 60, checkperiod: 120 });
